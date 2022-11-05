970f5658491a3dbf74c6b698053dccac9e6bdf7e
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-05 16:51:02 +0000

    responsive boxes

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,16 @@
-
+let columnNum = 2;
+let rowNum = columnNum;
 
 const gridContainer = document.querySelector(".gridContainer");
 
 function makeDivs(numDivs){
     for (let i=0; i<numDivs; i++) {
         let boxes = document.createElement('div');
+        gridContainer.style.gridTemplateColumns = `repeat(${columnNum},1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${columnNum},1fr)`;
         gridContainer.appendChild(boxes).className = "boxes";
     }
 }
 
 
-makeDivs((2*2));
+makeDivs((columnNum*rowNum));
