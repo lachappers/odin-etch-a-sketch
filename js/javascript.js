4282ea27a2d502797fff3eab2bb40ed0dece18af
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-05 23:13:27 +0000

    finished all functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,52 @@
 let columnNum = 5;
 let rowNum = columnNum;
 
+
 const gridContainer = document.querySelector(".gridContainer");
-
-
-function makeDivs(numDivs){
-    for (let i=0; i<numDivs; i++) {
-        let boxes = document.createElement('div');
-        gridContainer.style.gridTemplateColumns = `repeat(${columnNum},1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${columnNum},1fr)`;
-        gridContainer.appendChild(boxes).className = "boxes";
-    }
-}
+const resize = document.getElementById('resize');
+const boxes = document.querySelectorAll('.boxes');
 
 
 makeDivs((columnNum*rowNum));
 
-const boxes = document.querySelectorAll('.boxes');
 
-    boxes.forEach((box) => {
-        box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = 'blueviolet';
+resize.addEventListener('click', () => {
+    removeAllChildNodes(gridContainer);
+    let ask = prompt("Pick a size for the grid from 1-100", 50);
+        if ((ask > 0) && (ask < 101)){
+            let columnNum = ask;
+            let rowNum = ask;
+            makeDivs((columnNum*rowNum));
+        }
+        else {
+            alert(`Please enter a number between 1 and 100 to play`);
+        }
+    });
+
+function makeDivs(numDivs){
+    for (let i=0; i<numDivs; i++) {
+        let boxes = document.createElement('div');
+        gridContainer.style.gridTemplateColumns = `repeat(${Math.sqrt(numDivs)},1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${Math.sqrt(numDivs)},1fr)`;
+        gridContainer.appendChild(boxes).className = "boxes";
+    }
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = 'blueviolet';
+    });
+
+});
+
+const reset = document.getElementById('reset');
+    reset.addEventListener('click', () => {
+        boxes.forEach((box) => {
+        box.style.backgroundColor = 'transparent';
         });
     });
-        //     document.getElementsByClassName("boxes").style.backgroundColor = "red"; 
-        // });
-        // boxes[box].addEventListener('mouseout', (e) => {
-        //     e.target.style.background = 'black'; 
-        //     });
-    // });
-    // console.log(boxes);
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }}
